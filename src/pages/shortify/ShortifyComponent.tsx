49d39f4a88ce421813e8cea825480b0ffcfc9b9e
kwangjin.hwang <kwangjin.hwang>
2024-08-26 16:21:01 +0900

    update test

--- src/pages/shortify/ShortifyComponent.tsx
+++ src/pages/shortify/ShortifyComponent.tsx
@@ -99,24 +99,24 @@ const ShortifyComponent =  forwardRef((props: ShortifyComponentProps, ref) => {
       },
     }).result
     .then((data) => data.body.text())
-    // .then((text) => {
-    //   const vttfile = text.split("\n").slice(2);
-    //   const subtitleLength = vttfile.push("");
-    //   const subtitles: Subtitle[] = [];
+    .then((text) => {
+      const vttfile = text.split("\n").slice(2);
+      const subtitleLength = vttfile.push("");
+      const subtitles: Subtitle[] = [];
 
-    //   for(var i=0;i<subtitleLength/4; i++){
+      for(var i=0;i<subtitleLength/4; i++){
 
         
-    //     subtitles.push({
-    //       index: Number(vttfile[i*4]),
-    //       timestring: vttfile[i*4+1],
-    //       text: vttfile[i*4+2],
-    //     })
-    //   }
+        subtitles.push({
+          index: Number(vttfile[i*4]),
+          timestring: vttfile[i*4+1],
+          text: vttfile[i*4+2],
+        })
+      }
 
-    //   setSubtitles(subtitles);
+      setSubtitles(subtitles);
 
-    // })
+    })
     .catch(() => setFetchErr(true));
   }, [])
 
@@ -166,7 +166,7 @@ const ShortifyComponent =  forwardRef((props: ShortifyComponentProps, ref) => {
       })
 
       const converted = convertSections();
-      generateShort(converted, props.id, props.tab, title)
+      generateShort(converted, props.id, props.tab, '')
       .then(() => navigate(`/shorts/${props.id}/${props.tab}`));
     }
   }));
