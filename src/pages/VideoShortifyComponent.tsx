f7759cf0bb7290c1aaa80fbe93b16a7c513cd739
azjaehyun <azjaehyun@gmail.com>
2024-09-01 16:56:38 +0900

    shortoption add

--- src/pages/VideoShortifyComponent.tsx
+++ src/pages/VideoShortifyComponent.tsx
@@ -121,7 +121,7 @@ const VideoShortify: React.FC<VideoShortifyProps> = () => {
         {
           title: "Shortify highlight",
           content: (
-            stage > 2 ?
+            stage > 3 ?
             <ShortifyComponent id={id!} tab={selectedTab} title={highlightTitle} ref={childRef} />
             : <InProgressComponent />
           ),
