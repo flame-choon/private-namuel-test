6b6c3611e0fc278be1019e3c77384555de1f7639
kwangjin.hwang <kwangjin.hwang>
2024-09-07 19:10:18 +0900

    rever

--- src/pages/VideoShortifyComponent.tsx
+++ src/pages/VideoShortifyComponent.tsx
@@ -66,9 +66,8 @@ const VideoShortify: React.FC<VideoShortifyProps> = () => {
 
   const onSubmitHandler = () => {
     if(childRef.current){
-      alert('submit');
       setIsLoadingNextStep(true);
-      // childRef.current.submit();
+      childRef.current.submit();
     }
   }
 
